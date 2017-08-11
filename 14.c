942cf3df13c395edb72a371fbb69992e3f86d4ae
indhu0701 <30342676+indhu0701@users.noreply.github.com>
2017-08-11 16:21:14 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int i;
+ int k;
+ cout<<" enter number "<<endl;
+ cin>>i;
+ cout<<" enter number "<<endl;
+ cin>>k;
+ cout<<"*********************************"<<endl;
+ cout<<" programology student site "<<endl;
+ cout<<"**********************************"<<endl;
+ for(;i<=k;)
+ {
+ cout<<i<<endl;
+ //by programology
+ i=i+2;
+ }
+}
